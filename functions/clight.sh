93563ff0cfdf5c3d688f38e8847b30f449a2976d
brett <brettinternet@gmail.com>
2020-02-24 22:46:07 -0700

    fix clight func

--- functions/clight.sh
+++ functions/clight.sh
@@ -2,7 +2,7 @@
 
 
 function toggle_calibrate {
-    OFF=${1:false}
+    OFF=${1:=false}
     # Source: https://github.com/FedeDP/Clight/issues/50#issuecomment-491818912
     busctl --user set-property org.clight.clight /org/clight/clight/Conf org.clight.clight.Conf NoAutoCalib "b" "$OFF"
 }
