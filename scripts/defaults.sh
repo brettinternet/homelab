f73f29340fe0d488cdfe6271b6953aa8b6eca515
brett <brettinternet@gmail.com>
2020-12-27 00:51:35 -0700

    add git compat command with keyring

--- scripts/defaults.sh
+++ scripts/defaults.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# https://wiki.archlinux.org/index.php/Xdg-utils#xdg-settings
+# Set default web browser: https://wiki.archlinux.org/index.php/Xdg-utils#xdg-settings
 xdg-settings set default-web-browser chromium.desktop
 
-# https://wiki.archlinux.org/index.php/Nemo#Set_Nemo_as_default_file_browser
+# Set default file browser: https://wiki.archlinux.org/index.php/Nemo#Set_Nemo_as_default_file_browser
 xdg-mime default nemo.desktop inode/directory application/x-gnome-saved-search
 
-# https://wiki.archlinux.org/index.php/Nemo#Show_/_hide_desktop_icons
+# Hide desktop icons: https://wiki.archlinux.org/index.php/Nemo#Show_/_hide_desktop_icons
 gsettings set org.nemo.desktop show-desktop-icons false
 gsettings set org.nemo.preferences desktop-is-home-dir false
 
-# https://wiki.archlinux.org/index.php/Nemo#Change_the_default_terminal_emulator_for_Nemo
+# Set default terminal: https://wiki.archlinux.org/index.php/Nemo#Change_the_default_terminal_emulator_for_Nemo
 gsettings set org.cinnamon.desktop.default-applications.terminal exec i3-sensible-terminal
 
-# https://docs.npmjs.com/resolving-eacces-permissions-errors-when-installing-packages-globally
+# Set global NPM path: https://docs.npmjs.com/resolving-eacces-permissions-errors-when-installing-packages-globally
 npm config set prefix '~/.npm-global'
+
+# Use with Gnome-Keyring with Git credentials: https://wiki.archlinux.org/index.php/GNOME/Keyring#Git_integration
+git config --global credential.helper /usr/lib/git-core/git-credential-libsecret
